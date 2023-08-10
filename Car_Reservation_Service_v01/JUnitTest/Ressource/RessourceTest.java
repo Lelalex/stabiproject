df63f10b95550a502c78e2831220227af04cae71
Lelalex <leander.wendt@stud.leuphana.de>
2023-08-10 11:50:04 +0200

    Added Ressource Test

---
+++ Car_Reservation_Service_v01/JUnitTest/Ressource/RessourceTest.java
@@ -0,0 +1,14 @@
+package Ressource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RessourceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
