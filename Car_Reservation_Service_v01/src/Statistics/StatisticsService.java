1dbefa7d97ca2a7bfceeffdb5298d666e2042db3
Lelalex <leander.wendt@stud.leuphana.de>
2023-08-16 14:35:58 +0200

    added statistics pckg & service according to Prüfungsleistung

---
+++ Car_Reservation_Service_v01/src/Statistics/StatisticsService.java
@@ -0,0 +1,15 @@
+package Statistics;
+
+public class StatisticsService {
+
+	public StatisticsService() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void getGermanBookingsPaidByPaypal {};
+	public void getGermanBookingsPaidByMoneyWallet {};
+	public void getGermanBookingsPaidByGoogleWallet {};
+	public void getEnglishBookingsPaidByPaypal {};
+	public void getEnglishBookingsPaidByMoneyWallet {};
+	public void getEnglishBookingsPaidByGoogleWallet {};
+	
