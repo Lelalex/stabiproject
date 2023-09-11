d1603f3bc5a9e5f9dc0846253cbf1f2fdeefe7e4
Lelalex <134679182+Lelalex@users.noreply.github.com>
2023-09-11 10:55:06 +0200

    Update ConcreteBooking.java

--- Car_Reservation_Service_v01/src/Booking/ConcreteBooking.java
+++ Car_Reservation_Service_v01/src/Booking/ConcreteBooking.java
@@ -18,19 +18,19 @@ package Booking;
 
 
 public abstract class ConcreteBooking extends Booking {
-    private int bookingId;
-    private String bookingDetails;
-
-    public ConcreteBooking(int bookingId, String bookingDetails) {
-        this.bookingId = bookingId;
-        this.bookingDetails = bookingDetails;
-    }
-
-    public int getBookingId() {
-        return bookingId;
-    }
-
-    public String getBookingDetails() {
-        return bookingDetails;
-    }
+//    private int bookingId;
+//    private String bookingDetails;
+//
+//    public ConcreteBooking(int bookingId, String bookingDetails) {
+//        this.bookingId = bookingId;
+//        this.bookingDetails = bookingDetails;
+//    }
+//
+//    public int getBookingId() {
+//        return bookingId;
+//    }
+//
+//    public String getBookingDetails() {
+//        return bookingDetails;
+//    }
 }
