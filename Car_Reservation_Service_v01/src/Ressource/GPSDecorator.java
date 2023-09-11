d3c7aef65a7e98e89ea27753cc4115cf632088a8
Lelalex <134679182+Lelalex@users.noreply.github.com>
2023-09-11 11:04:04 +0200

    Update GPSDecorator.java

--- Car_Reservation_Service_v01/src/Ressource/GPSDecorator.java
+++ Car_Reservation_Service_v01/src/Ressource/GPSDecorator.java
@@ -1,18 +1,50 @@
 package Ressource;
 
+import java.util.Date;
 
 public class GPSDecorator extends Decorator {
-    public GPSDecorator(Ressource decorator) {
-        super(decorator);
+	private int duration; 
+	
+    public GPSDecorator(Ressource car) {
+        super(car);
     }
 
     @Override
     public String getDescription() {
-        return car.getDescription() + ", GPS Addon";
+        return car.getDescription() + ", Extra: GPS Addon";
     }
 
     @Override
     public Double getCost() {
         return car.getCost() + 10.0;
     }
+
+	@Override
+	public int getDuration() {
+		// TODO Auto-generated method stub
+		duration = car.getDuration();
+		return duration;
+	}
+
+	@Override
+	public void setDuration(int duration) {
+		this.duration = duration;
+		
+	}
+
+	@Override
+	public Double getTotalPrice() {
+		// TODO Auto-generated method stub
+		return car.getCost() * duration + 10.0 * duration;
+	}
+
+	@Override
+	public Date getStartDay() {
+		return car.getStartDay();
+	}
+
+	@Override
+	public void setStartDay(Date startDay) {
+		car.setStartDay(startDay);		
+	}
 }
