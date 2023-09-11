b828861b838df27b18bf955e0a0e4878740b0ff5
Lelalex <134679182+Lelalex@users.noreply.github.com>
2023-09-11 10:53:34 +0200

    Update Body.java

--- Car_Reservation_Service_v01/src/Booking/Body.java
+++ Car_Reservation_Service_v01/src/Booking/Body.java
@@ -1,47 +1,20 @@
 package Booking;
 
-import Ressource.Car;
+import Ressource.Ressource;
 
-//public class Body {
-//	
-//	private Car car;
-//	private int duration;
-//	private double price;
-//	private String startDay;
-//	
-//	public Body() {
-//		// TODO Auto-generated constructor stub
-//	}
-//	
-//	public void setBody(Car car, int duration, String startDay, Double cost) {
-//		// TODO addd CarConfig in Ressource
-//		
-//	}
-//	
-//}
 public class Body {
-    
-    private Car car;
-    private int duration;
-    private double price;
-    private String startDay;
-    
-    // Standardkonstruktor: Standardwerte für Instanzvariablen festgelegt
-    public Body() {
-        car = null;            // Standardwert für car: null (kein Auto zugewiesen)
-        duration = 0;          // Standardwert für duration: 0 (keine Dauer festgelegt)
-        price = 0.0;           // Standardwert für price: 0.0 (Preis auf 0.0 festgelegt)
-        startDay = "N/A";      // Standardwert für startDay: "N/A" (kein Startdatum festgelegt)
-    }
-    
-    public void setBody(Car car, int duration, String startDay, Double cost) {
-        this.car = car;
-        this.duration = duration;
-        this.startDay = startDay;
-        
-        // Berechnen des Preises basierend auf der Dauer und den Kosten des Autos
-        this.price = duration * cost;
-    }
+
+	private Ressource car;
+//    private int duration;
+//    private double price;
+//    private Date startDay;
+
+	// constructor with default valuees
+	public Body() {
+		car = null;
+	}
+
+	public void setBody(Ressource car) {
+		this.car = car;
+	}
 }
-
-
