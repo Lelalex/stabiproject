a1810dd98acdbfa890a2d5b81be7383ad1616e32
Lelalex <134679182+Lelalex@users.noreply.github.com>
2023-09-11 10:50:28 +0200

    Update AuthenticationService.java

--- Car_Reservation_Service_v01/src/Authentication/AuthenticationService.java
+++ Car_Reservation_Service_v01/src/Authentication/AuthenticationService.java
@@ -1,91 +1,86 @@
 package Authentication;
+
 import java.util.Scanner;
 import Person.Person;
+
 public class AuthenticationService {
-   private LoginStrategy strategy;
-   private boolean isAuthenticated;
-   private boolean authenticated = false;
-   private Person person;
-  
-   public void setStrategy(LoginStrategy strategy) {
-       this.strategy = strategy;
-   }
-  
-   public void setPerson(Person person) {
-       this.person = person;
-   }
-  
-   public void authenticateSubject() {
-       if(strategy != null && person != null) {
-           authenticated = strategy.runStrategy(person);
-       } else {
-           // Hier könnten Sie einen Fehler ausgeben oder eine Exception werfen,
-           // weil keine Strategie oder Person gesetzt wurde
-       }
-   }
-   // Hier überprüfen Sie, ob die Authentifizierung erfolgreich war
-   public boolean isAuthenticated() {
-       return authenticated;
-   }
-  
-  
-   public boolean authenticate(Person person) {
-       Scanner scanner = new Scanner(System.in);
-       System.out.print("Are you a natural or legal person? (n/l): ");
-       String type = scanner.nextLine();
-      
-       if ("n".equalsIgnoreCase(type)) {
-           System.out.println("1. Username with Password");
-           System.out.println("2. Fingerprint");
-           System.out.println("3. Eye scanner");
-           System.out.println("4. Go back to main menu");
-       } else if ("l".equalsIgnoreCase(type)) {
-           System.out.println("1. Username with Password");
-           System.out.println("2. Go back to main menu");
-       } else {
-           System.out.println("Invalid choice");
-           return false;
-       }
-       int choice = scanner.nextInt();
-       scanner.nextLine();  // Consume newline
-       switch (choice) {
-           case 1:
-           	strategy = new PasswordStrategy(new PasswordStrategy.ScannerInputProvider());
-               break;
-           case 2:
-               if ("n".equalsIgnoreCase(type)) {
-                   strategy = new FingerPrintStrategy();
-               } else {
-                   // Go back to the main menu
-                   return false;
-               }
-               break;
-           case 3:
-               if ("n".equalsIgnoreCase(type)) {
-                   strategy = new EyeScanStrategy();
-               } else {
-                   // Invalid choice
-                   System.out.println("Invalid choice");
-                   return false;
-               }
-               break;
-           case 4:
-               if ("n".equalsIgnoreCase(type)) {
-                   // Go back to the main menu
-                   return false;
-               }
-               break;
-           default:
-               System.out.println("Invalid choice");
-               return false;
-       }
-       if (strategy != null) {
-           return strategy.runStrategy(person);
-       }
-       return false;
-   }
+	private LoginStrategy strategy;
+	private boolean isAuthenticated;
+	private boolean authenticated = false;
+	private Person person;
+
+	public void setStrategy(LoginStrategy strategy) {
+		this.strategy = strategy;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	public void authenticateSubject() {
+		if (strategy != null && person != null) {
+			authenticated = strategy.runStrategy(person);
+		} else {
+		}
+	}
+
+	// Check if Auth was successfull
+	public boolean isAuthenticated() {
+		return authenticated;
+	}
+
+	public boolean authenticate(Person person) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Are you a natural or legal person? (n/l): ");
+		String type = scanner.nextLine();
+
+		if ("n".equalsIgnoreCase(type)) {
+			System.out.println("1. Username with Password");
+			System.out.println("2. Fingerprint");
+			System.out.println("3. Eye scanner");
+			System.out.println("4. Go back to main menu");
+		} else if ("l".equalsIgnoreCase(type)) {
+			System.out.println("1. Username with Password");
+			System.out.println("2. Go back to main menu");
+		} else {
+			System.out.println("Invalid choice");
+			return false;
+		}
+		int choice = scanner.nextInt();
+		scanner.nextLine(); 
+		switch (choice) {
+		case 1:
+			strategy = new PasswordStrategy(new PasswordStrategy.ScannerInputProvider());
+			break;
+		case 2:
+			if ("n".equalsIgnoreCase(type)) {
+				strategy = new FingerPrintStrategy();
+			} else {
+				// Go back to the main menu
+				return false;
+			}
+			break;
+		case 3:
+			if ("n".equalsIgnoreCase(type)) {
+				strategy = new EyeScanStrategy();
+			} else {
+				System.out.println("Invalid choice");
+				return false;
+			}
+			break;
+		case 4:
+			if ("n".equalsIgnoreCase(type)) {
+				// Go back to the main menu
+				return false;
+			}
+			break;
+		default:
+			System.out.println("Invalid choice");
+			return false;
+		}
+		if (strategy != null) {
+			return strategy.runStrategy(person);
+		}
+		return false;
+	}
 }
-
-
-
-
