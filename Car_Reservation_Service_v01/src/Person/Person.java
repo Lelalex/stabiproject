fb6f6f35ca60d37e9b838015fa9e83e421ee9865
Lelalex <134679182+Lelalex@users.noreply.github.com>
2023-09-11 11:02:09 +0200

    Update Person.java

--- Car_Reservation_Service_v01/src/Person/Person.java
+++ Car_Reservation_Service_v01/src/Person/Person.java
@@ -1,54 +1,64 @@
 package Person;
+
+import java.util.Scanner;
+
 import Payment.Account;
+
 public abstract class Person {
-   // Gemeinsame Eigenschaften und Methoden für alle Person-Typen
-   protected String name;
-   private String id;
-   protected Address address;
-   protected String eMail;
-   private String password;
-   private String userName;
-   public Person(String name, String id, Address address) {
-       this.name = name;
-       this.id = id;
-       this.address = address;
-   }
-   // Gemeinsame Methoden für alle Person-Typen
-   public void displayInfo() {
-       System.out.println("Name: " + name);
-       System.out.println("ID: " + id);
-       address.printAddress();
-   }
-  
-   public void setPassword(String password) {
-       this.password = password;
-   }
-   public String getPassword() {
-       return password;
-   }
-   public String getUserName() {
-       return this.userName;
-   }
-   public void setUserName(String userName) {
-       this.userName = userName;
-   }
-  
-   public String getID() {
-       return this.id;
-   }
-   public void setID(String id) {
-       this.id = id;
-   }
-   public abstract String getName();
-   public abstract Address getAddress();
-   public abstract String getMail();
-   public abstract Account getBankAccount();
-   public abstract void setBankAccount(Account account);
-  
-   public abstract String getIban();
-   public abstract void setIban(String Iban);
+
+	protected String name;
+	protected String id;
+	protected Address address;
+	protected String eMail;
+
+	public Person(String name, String id, Address address) {
+		this.name = name;
+		this.id = id;
+		this.address = address;
+	}
+
+	// constructor without parameters, scanning ID
+	protected Person() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter your ID: ");
+		this.id = scanner.nextLine();
+	}
+
+	public void displayInfo() {
+		System.out.println("Name: " + name);
+		System.out.println("ID: " + id);
+		address.printAddress();
+	}
+
+	public String getID() {
+		return this.id;
+	}
+
+	public void setID(String id) {
+		this.id = id;
+	}
+	
+    public abstract String getUserName();
+
+    public abstract void setUserName(String userName);
+
+	public abstract String getName();
+
+	public abstract void setName(String name);
+
+	public abstract Address getAddress();
+
+	public abstract String getMail();
+
+	public abstract Account getBankAccount();
+
+	public abstract void setBankAccount(Account account);
+
+	public abstract String getPassword();
+
+	public abstract void setPassword(String password);
+
+	public abstract String getIban();
+
+	public abstract void setIban(String Iban);
 }
-
-
-
-
