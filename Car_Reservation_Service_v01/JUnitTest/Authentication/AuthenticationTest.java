a34fdc736e9dfd84ff7949644815ce276d2178e1
Lelalex <leander.wendt@stud.leuphana.de>
2023-09-11 11:19:25 +0200

    Reupdate Authentication Test

--- Car_Reservation_Service_v01/JUnitTest/Authentication/AuthenticationTest.java
+++ Car_Reservation_Service_v01/JUnitTest/Authentication/AuthenticationTest.java
@@ -1,6 +1,7 @@
 package Authentication;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import Person.Address;
