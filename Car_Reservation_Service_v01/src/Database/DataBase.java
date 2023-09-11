a1424fe2ba20bc2960f0e1b145c5e152a6840a8b
Lelalex <134679182+Lelalex@users.noreply.github.com>
2023-09-11 11:00:02 +0200

    Update DataBase.java

--- Car_Reservation_Service_v01/src/Database/DataBase.java
+++ Car_Reservation_Service_v01/src/Database/DataBase.java
@@ -1,47 +1,46 @@
 package Database;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import Authentication.AuthenticationService;
 import Person.Person;
+
 public class DataBase {
-  
-   private static DataBase dbEntry;
-  
-   public Map<String, Person> Persons = new HashMap<>();
-   public ArrayList<AuthenticationService> Authentications = new ArrayList<>();
-  
-   static int PersonID = 1;
-  
-   private DataBase() {       
-   }
-  
-   public void savePerson(Person person) {
-       String personID = String.valueOf(PersonID);
-       person.setID(personID);
-       this.Persons.put(personID, person);
-       PersonID++;
-   }
-  
-   public Person getPerson(String id) {
-       return Persons.get(id);
-   }
-   public void saveAuthentication(AuthenticationService authenticationservice) {
-       this.Authentications.add(authenticationservice);
-   }
-  
-   public AuthenticationService getAuthentication() {
-       return Authentications.get(Authentications.size()-1);
-   }
-  
-   public static DataBase getInstance() {
-       if(dbEntry == null) {
-           dbEntry = new DataBase();
-       }
-       return dbEntry;
-   }
+
+	private static DataBase dbEntry;
+
+	public Map<String, Person> Persons = new HashMap<>();
+	public ArrayList<AuthenticationService> Authentications = new ArrayList<>();
+
+	static int PersonID = 1;
+
+	private DataBase() {
+	}
+
+	public void savePerson(Person person) {
+		String personID = String.valueOf(PersonID);
+		person.setID(personID);
+		this.Persons.put(personID, person);
+		PersonID++;
+	}
+
+	public Person getPerson(String id) {
+		return Persons.get(id);
+	}
+
+	public void saveAuthentication(AuthenticationService authenticationservice) {
+		this.Authentications.add(authenticationservice);
+	}
+
+	public AuthenticationService getAuthentication() {
+		return Authentications.get(Authentications.size() - 1);
+	}
+
+	public static DataBase getInstance() {
+		if (dbEntry == null) {
+			dbEntry = new DataBase();
+		}
+		return dbEntry;
+	}
 }
-
-
-
-
